26dd711be731f7f09fe993630102835b4161f699
wangyong <yundan_wy@126.com>
2018-05-13 22:09:39 +0800

    '添加freemarker模板支持'

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,11 +16,14 @@ public class IndexController {
 
 
   @RequestMapping("/index")
-  public String index() {
+  public String index(HttpServletRequest request) {
     List<Integer> integerList = Arrays.asList(1, 2, 3, 4);
     integerList.forEach(intValue -> {
       System.out.println(intValue);
     });
+
+    request.setAttribute("title", "首页");
+    request.setAttribute("user", "张三");
     return "index";
   }
 
